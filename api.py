43cede26c9315f76a620e41310596bfab5a56817
Alvaro de Landaluce <Landaluce@users.noreply.github.com>
2018-01-13 12:14:22 -0500

    Update api.py

--- api.py
+++ api.py
@@ -5,6 +5,6 @@ CONSUMER_SECRET = 'your_consumer_secret'
 ACCESS_TOKEN = 'your_access_token'
 ACCESS_TOKEN_SECRET = 'your_access_token_secret'
 
-authentication = tweepy.OAuthHandler(consumer_key, consumer_secret)
-authentication.set_access_token(access_token, access_token_secret)
+authentication = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+authentication.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(authentication)
